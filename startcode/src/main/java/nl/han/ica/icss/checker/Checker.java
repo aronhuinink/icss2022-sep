2a9b033e5be1d2f16f0daca7c06b68887e30a466
aronhuinink <151731688+aronhuinink@users.noreply.github.com>
2026-02-04 15:35:04 +0100

    vgm CH01 gemaakt, alleen nog in de code zetten

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -5,13 +5,22 @@ import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Objects;
 
 
 public class Checker {
 
     private HANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    ArrayList<String> variableNames = new ArrayList<>();
+
+    /*
+    TODO
+    Check of variables daadwerkelijk gedefinieerd zijn
+    stap 1: sla de namen van de variabele op in een lijst V
+    stap 2: vergelijk de namen van de variabelen op de plekken waar ze gebruikt worden
+     */
 
 
     public void check(AST ast) {
@@ -23,20 +32,33 @@ public class Checker {
         System.out.println("--------------------------------------------------------");
         variableTypes.addFirst(new HashMap<>());
         for (ASTNode child : root.getChildren()) {
-            if(child instanceof VariableReference) {
-                System.out.println("children are instances of variablereference");
-                checkVariableReference((VariableReference) child);
+            if(child instanceof VariableAssignment) {
+                for (ASTNode grandChild : child.getChildren()) {
+                    if(grandChild instanceof VariableReference){
+                        addVariableToList((VariableReference) grandChild);
+                    }
+                }
             }
         }
+
+        for (int i = 0; i < variableNames.size(); i++) {
+            System.out.println(variableNames.get(i));
+        }
+
+    }
+
+    private void addVariableToList(VariableReference node) {
+        String variableName = node.name;
+        variableNames.add(variableName);
     }
 
     private void checkVariableReference(VariableReference node) {
-        String variableName = node.name;
-        for(int i = 0; i < variableTypes.getSize(); i++){
-
+        for (String name : variableNames) {
+            if (Objects.equals(name, node.name)) {
+                return;
+            }
         }
+        node.setError("Variable not found");
     }
-
-
 
 }
