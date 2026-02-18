8b844ccf0a93049f34195f35cb6aa92b07854cb4
aronhuinink <151731688+aronhuinink@users.noreply.github.com>
2026-02-18 13:56:00 +0100

    Update Generator.java

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -2,14 +2,32 @@ package nl.han.ica.icss.generator;
 
 
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.ASTNode;
+import nl.han.ica.icss.ast.Stylerule;
+import nl.han.ica.icss.ast.Stylesheet;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        return "";
-
-
+		return generateStylesheet(ast.root);
 	}
 
-	
+	private String generateStylesheet(Stylesheet stylesheet){
+		String result = "";
+
+		for (ASTNode node : stylesheet.getChildren()) {
+			if (node instanceof Stylerule) {
+				result += generateStylerule((Stylerule) node);
+			}
+		}
+		return result;
+	}
+
+	private String generateStylerule(Stylerule node){
+		String outcome = "";
+
+
+
+		return outcome;
+	}
 }
